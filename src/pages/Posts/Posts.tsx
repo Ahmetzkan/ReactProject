408b40c9d2d90cd58294d0a0b743ed992df50db2
Ahmet Özkan <70511583+Ahmetzkan@users.noreply.github.com>
2024-01-15 16:47:26 +0300

    React-Redux-Axios-ContextAPI-Local Storage-Json2ts-Typescript migration- Thunk-DevTools-Configure-Interceptors

---
+++ src/pages/Posts/Posts.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {cartActions} from "../../store/slices/cartSlice";
+import {getAllPosts} from "../../store/slices/postSlice";
+import PostCard from "../../components/PostCard/PostCard";
+import {Post} from "../../models/post";
+function Posts() {
+	const dispatch = useDispatch();
+
+	const postState = useSelector((state: any) => state.post);
+	console.log(postState);
+	useEffect(() => {
+		dispatch(getAllPosts() as any);
+	}, []);
+
+	return (
+		<div>
+			Posts
+			{postState.posts.map((post: Post) => (
+				<PostCard post={post} />
+			))}
+		</div>
+	);
+}
+
+export default Posts;
---
+++ src/services/postService.ts
@@ -0,0 +1,15 @@
+import {AxiosResponse} from "axios";
+import {Post} from "../models/post";
+import axiosInstance from './../utils/axiosInterceptor';
+
+class PostService {
+	getAll(): Promise<AxiosResponse<Post[], any>> {
+		return axiosInstance.get<Post[]>("posts");
+	}
+
+	getById(id: number): Promise<AxiosResponse<Post, any>> {
+		return axiosInstance.get<Post>("posts/" + id);
+	}
+}
+
+export default new PostService();
